ebe6c6314c4711cd7256e794693dcb8e2bdafe84
RAY-PC\ray <ramon.polidura@gmail.com>
2020-05-19 17:44:01 +0100

    Set up axios file helper

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,35 +2,25 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <p>Count of the games: {{ games.length }}</p>
+    <div v-for="({name}, key) in games" v-bind:key="key">{{ key }}. {{ name }}</div>
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import axios from 'axios';
+import { Component, Vue } from "vue-property-decorator";
+import axiosHelper from "../helpers/axios";
+import axios from "axios";
 
 @Component({
   components: {}
-  })
+})
 export default class Home extends Vue {
   games = [];
 
-  mounted() {
-    this.games = [];
-    axios('https://api.rawg.io/api/games', {
-      method: 'GET',
-      params: {
-        ordering: 'popularity',
-        page_size: 10,
-        platforms: 167,
-      },
-    })
-      .then((response) => {
-        this.games = response.data.results;
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+  async mounted() {
+    const axiosInstance = new axiosHelper();
+    this.games = (await axiosInstance.getGames()) as any;
+    console.log(this.games);
   }
 }
 </script>

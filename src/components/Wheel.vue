e947c56a151b114ddeef09370c71678b417fd88b
RAY-PC\ray <ramon.polidura@gmail.com>
2020-05-20 18:11:47 +0100

    Add wheel component and styles

---
+++ src/components/Wheel.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <div v-for="({name}, key) in games" v-bind:key="key">{{ key + 1 }}. {{ name }}</div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+@Component({
+  components: {}
+})
+export default class Wheel extends Vue {
+  @Prop({ required: true }) games!: any[];
+  async mounted() {}
+}
+</script>
+<style lang="scss">
+// @import "../styles/wheel.scss";
+</style>

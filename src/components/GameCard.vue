879a604afa5740c8a282da2c6a14c2fcc59aec57
RAY-PC\ray <ramon.polidura@gmail.com>
2020-06-08 15:47:01 +0100

    Add styles

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -6,18 +6,32 @@
         <img v-if="game.background_image" class="gameCard__image" :src="game.background_image" />
       </div>
     </div>
-    <div class="gameCard__rating">{{`${game.rating} out of 5`}}</div>
-    <div class="gameCard__release-date"></div>
+    <div class="gameCard__content">
+      <div class="gameCard__property">
+        <div class="gameCard__property-label">Rating: </div>
+        <div class="gameCard__property-value">{{`${game.rating} out of 5`}}</div>
+      </div>
+      <div class="gameCard__property">
+        <div class="gameCard__property-label">Release date: </div>
+        <div class="gameCard__property-value">{{game.released}}</div>
+      </div>
+      <div class="gameCard__property">
+        <div class="gameCard__property-label">Genres: </div>
+        <div class="gameCard__property-value"
+          v-for="(genre, key) in game.genres"
+          v-bind:key="key">{{`${key > 0 ? ', ' : ''}${genre.name}`}}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
-  import { Component, Vue, Prop } from 'vue-property-decorator';
-  @Component({
+import { Component, Vue, Prop } from 'vue-property-decorator';
+@Component({
   components: {}
   })
-  export default class GameCard extends Vue {
+export default class GameCard extends Vue {
     @Prop({ required: true }) game!: any;
-  }
+}
 </script>
 <style lang="scss">
   @import "../styles/gameCard.scss";

112cea0ffe0d612280ecc6077097c007295fb9f9
Ray Polidura <ray@heydoc.co.uk>
2020-06-19 15:38:30 +0100

    Bump cli-service

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'production'
-//     ? '/mega-drive-games/'
-//     : '/'
-// }
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/mega-drive-games/'
+    : '/'
+}
